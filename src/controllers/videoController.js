d34f201a46e1010171aa9fd49b1479e61197305b
minhosong88 <hominsong@naver.com>
2023-06-10 14:08:07 -0700

    adding Search page
    built search page with regular expression

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video.js"
 
 
 export const home = async(req, res) => {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({createdAt:-1});
     return res.render("home",{pageTitle:"Home", videos});
 
 };
@@ -65,4 +65,18 @@ export const deleteVideo =async(req, res) =>{
     const {id} = req.params;
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
+}
+
+export const search = async(req, res) =>{
+    const {keyword} = req.query;
+    let videos = [];
+    if(keyword){
+       videos = await Video.find({
+          title:{ 
+            $regex: new RegExp(keyword, "i"),
+        },
+        });
+    }
+    console.log(videos);
+    return res.render("search",{pageTitle:"Search", videos});
 }
