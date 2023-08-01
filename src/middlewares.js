d74744fc7495c31c300172aa204e37cd63663c92
minhosong88 <hominsong@naver.com>
2023-07-31 20:18:46 -0700

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -13,7 +13,8 @@ const multerUploader = multerS3({
     s3:s3,
     bucket: "wetubenomad",
     acl: "public-read",
-})
+    contentType: multerS3.AUTO_CONTENT_TYPE
+});
 
 export const localsMiddleware = (req, res, next) => {
     
