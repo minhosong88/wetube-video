90cea3394b04a0b7959f852f889427b52fb3a68a
minhosong88 <hominsong@naver.com>
2023-08-05 18:44:12 -0700

    Update videoPlayer.js

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -16,7 +16,7 @@ let controlsTimeout = null;
 let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
-console.log(video.duration);
+console.log(loadedMetadata);
 const volumeIconsChange = () => {
     if(video.volume > 0 && video.volume < 0.4 ){
         return muteBtnIcon.classList = "fas fa-volume-off";
